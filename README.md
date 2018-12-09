7be516c3e27005beeb11ab72cc02ceeb101b9b3c
eggyolktech <eggyolktech@gmail.com>
2018-12-09 22:10:12 +0800

    Bulk commit of missing JS and images

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # homeBootStrap4
+
+ln -s /app/homeBootStrap4/index.html index.html
+ln -s /app/homeBootStrap4/css css
+ln -s /app/homeBootStrap4/images images
+ln -s /app/homeBootStrap4/js js
+
---
+++ js/common.js
@@ -0,0 +1,15 @@
+
+var tfPeriod = {
+    list:[{"timeframe": "M", "period": "2060"}, {"timeframe": "W", "period": "11"}, {"timeframe": "D", "period": "9"}, {"timeframe": "h", "period": "5023"}, {"timeframe": "m", "period": "5012"}]
+}
+
+var tfCNPeriod = {
+    list:[{"timeframe": "M", "period": "54"}, {"timeframe": "W", "period": "11"}, {"timeframe": "D", "period": "9"}, {"timeframe": "h", "period": "5023"}, {"timeframe": "m", "period": "5012"}]
+}
+
+var tfUSPeriod = {
+    list:[{"timeframe": "M", "period": "54"}, {"timeframe": "W", "period": "11"}, {"timeframe": "D", "period": "9"}, {"timeframe": "h", "period": "4"}, {"timeframe": "m", "period": "0"}]
+}
+
+var CAPTION_SELECT = "請選擇";
+var CAPTION_SELECT_EN = "Please Select";
